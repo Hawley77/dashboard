0738cd605bf7d9060f9e31516cfa453dba125464
Kevin Ramos Dos Santos De Oliveira <kevram52@outlook.com>
2022-05-22 21:46:18 -0300

    Home page finished

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = document.querySelector("#root");
+ReactDOM.render(<App />, root);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,9 @@
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Navbar from "../../components/Navbar/Navbar";
 import Widget from "../../components/Widget/Widget";
+import Featured from "../../components/Featured/Featured";
+import Chart from "../../components/Chart/Chart";
+import Table from "../../components/Table/Table";
 import "./Home.scss";
 
 const Home = () => {
@@ -14,6 +17,14 @@ const Home = () => {
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
         </div>
       </div>
     </div>
